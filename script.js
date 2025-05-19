efbe668d2b7b146347bd6d3151c50a0f9810e112
ChivuAndrei2003 <chivuandei2003@gmail.com>
2025-05-19 19:13:39 +0300

    update

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 function Gameboard(){
   const gameboard = [];
+  const rows = 3;
+  const columns = 3;
+  getUser = '';
   
-  for(let i = 0; i < 3; i++){
+  for(let i = 0; i < rows; i++) {
     gameboard[i]=[];
-    for(let j = 0; j < 3; j++){      //this creates an empty 3x3 string 
+    for(let j = 0; j < columns; j++){   //this creates an 3x3 array with Cell() on every line & column
       gameboard[i].push(Cell());
     } 
    }
-  
-  const  createUser = (name, sign,position) =>{
+                                          //getGameboard() is equivalent to :
+  const getGameboard = () => gameboard;   //const board = Gameboard();
+                                          //board.getGameboard();
+   
+  const getAvailableCells = () => {
+    const cells = [];
+      for(let i=0; i < rows ; i++){
+        for(let j=0 ; j < columns ; j++){
+          if(gameboard[i][j].getValue() === "") {
+            cells.push([i,j]);
+          }
+        }
+      }
+      return cells;
+  };
 
-
+  const printBoard = () => {
+    const boardWithCellValue = gameboard.map((rows) => rows.map((cell) => 
+     cell.getValue()))
+    console.log(boardWithCellValue);
   };
   
-
-  
- // this.switchUser = 
-
-
-
-    
+  return{getGameboard,getAvailableCells,printBoard};
 }
-Gameboard();
+
+//board.gameboard[0][0].setValue("X");        // acces direct
 
 function GameController(){
   
@@ -31,6 +45,15 @@ function GameController(){
 
 
 
+/*
+const dropSign = () => {
+   
+    const availableCells = gameboard.forEach(element => {
+   
+      if(element)
+
+   });
+  }
 
 
 
@@ -44,6 +67,4 @@ function GameController(){
 
 
 
-
-
-
+*/
